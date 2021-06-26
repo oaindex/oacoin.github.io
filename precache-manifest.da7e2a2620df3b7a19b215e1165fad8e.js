7264254b99fef75202936476f4a74abda63074d4
monkbi <monktoken2021@163.com>
2021-06-27 03:31:25 +0800

    first commit

---
+++ precache-manifest.da7e2a2620df3b7a19b215e1165fad8e.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b82103d527681551ea",
+    "url": "/css/chunk-19cd7b55.e18c6c0d.css"
+  },
+  {
+    "revision": "60fee75df9a71793f869",
+    "url": "/css/chunk-7c6c94b4.48900726.css"
+  },
+  {
+    "revision": "17fb36cc1558af4d8990",
+    "url": "/css/chunk-7e2637df.eec2fda2.css"
+  },
+  {
+    "revision": "172ed12166e642367e6a",
+    "url": "/css/chunk-91e45e1e.1890eed6.css"
+  },
+  {
+    "revision": "5710dbd5732af9ba2346",
+    "url": "/css/chunk-cf8bbff8.11134797.css"
+  },
+  {
+    "revision": "064d678f7aa5ced17711",
+    "url": "/css/chunk-vendors.62353d31.css"
+  },
+  {
+    "revision": "7ca35740fc213ed084b1",
+    "url": "/css/index.269a57c4.css"
+  },
+  {
+    "revision": "11c302dd8c50619e4131da5d645fb422",
+    "url": "/fonts/SourceHanSansCN-Regular.11c302dd.otf"
+  },
+  {
+    "revision": "4bd25845aa2f1ad608e8ca01f934181d",
+    "url": "/images/change.png"
+  },
+  {
+    "revision": "1f938d1a5908396ba24c5c96fd0049d1",
+    "url": "/images/oa.png"
+  },
+  {
+    "revision": "114192e268426781b4d0b7b82aa89775",
+    "url": "/images/usdt.png"
+  },
+  {
+    "revision": "982994c20a909625908114c412b572e0",
+    "url": "/img/aoyun-bg.982994c2.png"
+  },
+  {
+    "revision": "b1bff5fb7387c82c4d9381573826b70c",
+    "url": "/img/aoyun-bg2.b1bff5fb.png"
+  },
+  {
+    "revision": "984dfdcfdf8652066e87b9156be62bf0",
+    "url": "/img/bind-address-bg-en.984dfdcf.png"
+  },
+  {
+    "revision": "57052cce09838de14cdae929d1e0b06a",
+    "url": "/img/bind-address-bg-ja.57052cce.png"
+  },
+  {
+    "revision": "98ee0f7f7ddbbb710cbb2a2912941450",
+    "url": "/img/bind-address-bg.98ee0f7f.png"
+  },
+  {
+    "revision": "40f337324abd10e45f96274eb90ff01a",
+    "url": "/img/culture-bg1.40f33732.png"
+  },
+  {
+    "revision": "22ae72c8e27b8b5d6dca0a4b14ecaf07",
+    "url": "/img/culture-bg2.22ae72c8.png"
+  },
+  {
+    "revision": "e942b4a44ec7618348cb61b92535ca12",
+    "url": "/img/footer-logo.e942b4a4.png"
+  },
+  {
+    "revision": "bdfcc28232f8a6936f28cc7a7dd718e6",
+    "url": "/img/frame-1555@2.bdfcc282.png"
+  },
+  {
+    "revision": "30465a6b848a6263a7a8da9c5b29433e",
+    "url": "/img/head-bg.30465a6b.png"
+  },
+  {
+    "revision": "aa34940600ff2a76d724d6262b2cb1d6",
+    "url": "/img/huoju@2x.aa349406.png"
+  },
+  {
+    "revision": "f57172ffef43398466c0f4dc2feda360",
+    "url": "/img/logo-c.f57172ff.png"
+  },
+  {
+    "revision": "1adb02ddde30626231a54d63a284b54e",
+    "url": "/img/logo@2x.1adb02dd.png"
+  },
+  {
+    "revision": "0ae36a12a8d723a2da3efc8641a2a99b",
+    "url": "/img/transmit-bg-en.0ae36a12.png"
+  },
+  {
+    "revision": "fcff75bc1d64b534cd91b66c9ae35b23",
+    "url": "/img/transmit-bg-ja.fcff75bc.png"
+  },
+  {
+    "revision": "81e71a57c0e64a4378161466b52577f7",
+    "url": "/img/transmit-bg.81e71a57.png"
+  },
+  {
+    "revision": "de6947b9645f68402b5cd30d7bd68a4b",
+    "url": "/img/video-play-icon@2x.de6947b9.png"
+  },
+  {
+    "revision": "242d6aeb4bca9b14aaef7a6923fc35cb",
+    "url": "/img/zh-icon.242d6aeb.png"
+  },
+  {
+    "revision": "00661d0bd018640cc71628b8938fcb1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8b82103d527681551ea",
+    "url": "/js/chunk-19cd7b55.85a96873.js"
+  },
+  {
+    "revision": "60fee75df9a71793f869",
+    "url": "/js/chunk-7c6c94b4.40c07e53.js"
+  },
+  {
+    "revision": "17fb36cc1558af4d8990",
+    "url": "/js/chunk-7e2637df.22a2c01f.js"
+  },
+  {
+    "revision": "172ed12166e642367e6a",
+    "url": "/js/chunk-91e45e1e.af80f00e.js"
+  },
+  {
+    "revision": "5710dbd5732af9ba2346",
+    "url": "/js/chunk-cf8bbff8.0fadcace.js"
+  },
+  {
+    "revision": "064d678f7aa5ced17711",
+    "url": "/js/chunk-vendors.2b6f15bc.js"
+  },
+  {
+    "revision": "7ca35740fc213ed084b1",
+    "url": "/js/index.81f61cb5.js"
+  },
+  {
+    "revision": "336910db135aa003a6974fd13b0449f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3d59030114e2025e700bc4f77decea41",
+    "url": "/whitepaper/OACoinWhitepaper_en.pdf"
+  },
+  {
+    "revision": "1250099502b462c7bc380ca7ca17c444",
+    "url": "/whitepaper/OACoinWhitepaper_jp.pdf"
+  },
+  {
+    "revision": "a8f15c7021e00a141e89a98ec46d6ae2",
+    "url": "/whitepaper/OACoinWhitepaper_zh.pdf"
+  }
+]);
